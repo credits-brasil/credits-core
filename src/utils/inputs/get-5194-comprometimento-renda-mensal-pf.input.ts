1ed5a7f6b08d045fc1c952e09f0cb81925f79909
Matheus Santos <devops@creditsbrasil.com.br>
2026-08-05 18:52:20 -0300

    feat: finish insumos PF

--- src/utils/inputs/get-5194-comprometimento-renda-mensal-pf.input.ts
+++ src/utils/inputs/get-5194-comprometimento-renda-mensal-pf.input.ts
@@ -1,8 +1,37 @@
 export const get5194ComprometimentoRendaMensalPfInput =
   (comprometimentoRendaMensalPf: {
-    resumo: {
-      $: {
-        "quantidade-total": string;
+    resumo?: {
+      $?: {
+        "quantidade-total"?: string;
       };
     };
-  }) => comprometimentoRendaMensalPf.resumo.$["quantidade-total"];
+    "detalhe-comprometimento-renda-mensal-pf"?: {
+      "classificacao-endividamento"?: string;
+      faixa?: string;
+      "flag-endividamento"?: string;
+      percentual?: string;
+    };
+  }) => ({
+    resumo: {
+      "quantidade-total":
+        comprometimentoRendaMensalPf?.resumo?.$?.["quantidade-total"] ?? "0",
+    },
+    "detalhe-comprometimento-renda-mensal-pf": {
+      "classificacao-endividamento":
+        comprometimentoRendaMensalPf?.[
+          "detalhe-comprometimento-renda-mensal-pf"
+        ]?.["classificacao-endividamento"],
+      faixa:
+        comprometimentoRendaMensalPf?.[
+          "detalhe-comprometimento-renda-mensal-pf"
+        ]?.faixa,
+      "flag-endividamento":
+        comprometimentoRendaMensalPf?.[
+          "detalhe-comprometimento-renda-mensal-pf"
+        ]?.["flag-endividamento"],
+      percentual:
+        comprometimentoRendaMensalPf?.[
+          "detalhe-comprometimento-renda-mensal-pf"
+        ]?.percentual,
+    },
+  });
--- src/utils/inputs/get-5262-alerta-identidade-fraude.input.ts
+++ src/utils/inputs/get-5262-alerta-identidade-fraude.input.ts
@@ -1,7 +1,39 @@
 export const get5262AlertaIdentidadeFraudeInput = (alertaIdentidadeFraude: {
-  resumo: {
-    $: {
-      "quantidade-total": string;
+  resumo?: {
+    $?: {
+      "quantidade-total"?: string;
     };
   };
-}) => alertaIdentidadeFraude.resumo.$["quantidade-total"];
+  "detalhe-alerta-identidade-fraude"?:
+    | {
+        "alerta-fraude"?: string;
+      }
+    | {
+        "alerta-fraude"?: string;
+      }[];
+}): {
+  resumo: {
+    "quantidade-total"?: string;
+  };
+  "detalhe-alerta-identidade-fraude": {
+    "alerta-fraude"?: string;
+  }[];
+} => ({
+  resumo: {
+    "quantidade-total": alertaIdentidadeFraude?.resumo?.$?.["quantidade-total"],
+  },
+  "detalhe-alerta-identidade-fraude": (() => {
+    const detalhes =
+      alertaIdentidadeFraude?.["detalhe-alerta-identidade-fraude"];
+
+    const lista = Array.isArray(detalhes)
+      ? detalhes
+      : detalhes
+        ? [detalhes]
+        : [];
+
+    return lista.map((item) => ({
+      "alerta-fraude": item["alerta-fraude"],
+    }));
+  })(),
+});
