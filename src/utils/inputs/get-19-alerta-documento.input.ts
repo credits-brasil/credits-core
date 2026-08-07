fceed1a44552cc1bb46c2df2cbca78a34b8ae7f9
Matheus Santos <devops@creditsbrasil.com.br>
2026-08-07 12:59:17 -0300

    fix: replace valid func

--- src/utils/inputs/get-19-alerta-documento.input.ts
+++ src/utils/inputs/get-19-alerta-documento.input.ts
@@ -1,7 +1,62 @@
 export const get19AlertaDocumentoInput = (alertaDocumento: {
-  resumo: {
-    $: {
-      "quantidade-total": string;
+  resumo?: {
+    $?: {
+      "quantidade-total"?: string;
+      "data-ultima-ocorrencia"?: string;
     };
   };
-}) => alertaDocumento.resumo.$["quantidade-total"];
+  "detalhe-alerta-documento"?:
+    | {
+        $?: {
+          "data-inclusao"?: string;
+          "data-ocorrencia"?: string;
+          "entidade-origem"?: string;
+          motivo?: string;
+          observacao?: string;
+        };
+        "tipo-documento-alerta"?: {
+          $?: {
+            nome?: string;
+          };
+        };
+      }
+    | {
+        $?: {
+          "data-inclusao"?: string;
+          "data-ocorrencia"?: string;
+          "entidade-origem"?: string;
+          motivo?: string;
+          observacao?: string;
+        };
+        "tipo-documento-alerta"?: {
+          $?: {
+            nome?: string;
+          };
+        };
+      }[];
+}) => ({
+  resumo: {
+    "quantidade-total": alertaDocumento?.resumo?.$?.["quantidade-total"] ?? "0",
+    "data-ultima-ocorrencia":
+      alertaDocumento?.resumo?.$?.["data-ultima-ocorrencia"],
+  },
+
+  "detalhe-alerta-documento": (() => {
+    const detalhes = alertaDocumento?.["detalhe-alerta-documento"];
+
+    const lista = Array.isArray(detalhes)
+      ? detalhes
+      : detalhes
+        ? [detalhes]
+        : [];
+
+    return lista.map((item) => ({
+      "data-inclusao": item.$?.["data-inclusao"],
+      "data-ocorrencia": item.$?.["data-ocorrencia"],
+      "entidade-origem": item.$?.["entidade-origem"],
+      motivo: item.$?.motivo,
+      observacao: item.$?.observacao,
+      "tipo-documento-alerta": item["tipo-documento-alerta"]?.$?.nome,
+    }));
+  })(),
+});
